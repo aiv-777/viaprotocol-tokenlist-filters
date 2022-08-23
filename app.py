1f57de960fe9ec53d7043dc825d88b7915d3da28
idagenyi <unleashedthesame@yahoo.com>
2022-08-24 01:16:48 +0300

    feat. filter by network type (mainnet or testnet) added

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ if __name__ == '__main__':
 
     # examples below
     filters.filter_by_symbol(symbol='usdt')
+    filters.filter_by_symbol(symbol='usdt', mainnet=True)
+    filters.filter_by_symbol(symbol='usdt', mainnet=False)
+
     filters.filter_by_chain_id(chain_id=56)
+
     filters.filter_by_sources(sources_list=['uniswap', '1inch'])
+    filters.filter_by_sources(sources_list=['uniswap', '1inch'], mainnet=True)
+    filters.filter_by_sources(sources_list=['uniswap', '1inch'], mainnet=False)
 
     # extra: for stablecoins containing 'usd' in this case
     filters.search_symbol_matches(symbol='usd')
+    filters.search_symbol_matches(symbol='usd', mainnet=True)
+    filters.search_symbol_matches(symbol='usd', mainnet=False)
--- filters.py
+++ filters.py
@@ -1,8 +1,10 @@
 import json
-from typing import Union
+from typing import Optional
+
+import networks
 
 
-def search_symbol_matches(symbol: str):
+def search_symbol_matches(symbol: str, mainnet: Optional[bool] = None):
 
     with open('all.json', mode='r') as raw:
         chains = json.loads(raw.read())
@@ -10,16 +12,32 @@ def search_symbol_matches(symbol: str):
     filtered_list = []
     for chain_id in chains:
         token_list = chains.get(chain_id)
-        for token in token_list:
-            ticker = token.get('symbol')
-            if symbol.lower() in ticker.lower():
-                filtered_list.append(token)
+        if mainnet:
+            if chain_id in networks.MAINNET.values():
+                for token in token_list:
+                    ticker = token.get('symbol')
+                    if symbol.lower() in ticker.lower():
+                        filtered_list.append(token)
+        elif mainnet is False:
+            if chain_id in networks.TESTNET.values():
+                for token in token_list:
+                    ticker = token.get('symbol')
+                    if symbol.lower() in ticker.lower():
+                        filtered_list.append(token)
+        elif mainnet is None:
+            for token in token_list:
+                ticker = token.get('symbol')
+                if symbol.lower() in ticker.lower():
+                    filtered_list.append(token)
 
-    description = '_symbol_match_' + symbol
+    if mainnet is None:
+        description = '_symbol_match_' + symbol
+    else:
+        description = '_symbol_match_' + symbol + f'_[{mainnet=}]'
     _write_to_json(description, filtered_list)
 
 
-def filter_by_symbol(symbol: str):
+def filter_by_symbol(symbol: str, mainnet: Optional[bool] = None):
 
     with open('all.json', mode='r') as raw:
         chains = json.loads(raw.read())
@@ -27,12 +45,28 @@ def filter_by_symbol(symbol: str):
     filtered_list = []
     for chain_id in chains:
         token_list = chains.get(chain_id)
-        for token in token_list:
-            ticker = token.get('symbol')
-            if ticker.lower() == symbol.lower():
-                filtered_list.append(token)
+        if mainnet:
+            if chain_id in networks.MAINNET.values():
+                for token in token_list:
+                    ticker = token.get('symbol')
+                    if ticker.lower() == symbol.lower():
+                        filtered_list.append(token)
+        elif mainnet is False:
+            if chain_id in networks.TESTNET.values():
+                for token in token_list:
+                    ticker = token.get('symbol')
+                    if ticker.lower() == symbol.lower():
+                        filtered_list.append(token)
+        elif mainnet is None:
+            for token in token_list:
+                ticker = token.get('symbol')
+                if ticker.lower() == symbol.lower():
+                    filtered_list.append(token)
 
-    description = '_symbol_' + symbol
+    if mainnet is None:
+        description = '_symbol' + symbol
+    else:
+        description = '_symbol' + symbol + f'_[{mainnet=}]'
     _write_to_json(description, filtered_list)
 
 
@@ -41,12 +75,13 @@ def filter_by_chain_id(chain_id: int):
     with open('all.json', mode='r') as raw:
         chains = json.loads(raw.read())
 
+    chain_name = networks.ALL.get(str(chain_id))
     filtered_list = chains.get(str(chain_id))
-    description = '_chain_id_' + str(chain_id)
+    description = '_chain_id_' + str(chain_id) + f'_[{chain_name=}]'
     _write_to_json(description, filtered_list)
 
 
-def filter_by_sources(sources_list: list):
+def filter_by_sources(sources_list: list, mainnet: Optional[bool] = None):
 
     with open('all.json', mode='r') as raw:
         chains = json.loads(raw.read())
@@ -54,16 +89,38 @@ def filter_by_sources(sources_list: list):
     filtered_list = []
     for chain_id in chains:
         token_list = chains.get(chain_id)
-        for token in token_list:
-            listed_in = token.get('listedIn')
-            for source in sources_list:
-                if source in listed_in:
-                    filtered_list.append(token)
-                    break
+        if mainnet:
+            if chain_id in networks.MAINNET.values():
+                for token in token_list:
+                    listed_in = token.get('listedIn')
+                    for source in sources_list:
+                        if source in listed_in:
+                            filtered_list.append(token)
+                            break
+        elif mainnet is False:
+            if chain_id in networks.TESTNET.values():
+                for token in token_list:
+                    listed_in = token.get('listedIn')
+                    for source in sources_list:
+                        if source in listed_in:
+                            filtered_list.append(token)
+                            break
+        elif mainnet is None:
+            for token in token_list:
+                listed_in = token.get('listedIn')
+                for source in sources_list:
+                    if source in listed_in:
+                        filtered_list.append(token)
+                        break
 
     description = ''
-    for source in sources_list:
-        description += '_' + source
+    if mainnet is None:
+        for source in sources_list:
+            description += '_' + source
+    else:
+        for source in sources_list:
+            description += '_' + source
+        description += f'_[{mainnet=}]'
     _write_to_json(description, filtered_list)
 
 
